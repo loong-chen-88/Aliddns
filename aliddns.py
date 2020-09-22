ad17b756728605661f8ab655dd30366d5b66575c
Loong Chen <szsn682871@gmail.com>
2020-09-23 01:17:32 +0800

    Initial commit

---
+++ aliddns.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import json
+from requests import get
+from aliyunsdkcore.client import AcsClient
+from aliyunsdkcore.acs_exception.exceptions import ClientException
+from aliyunsdkcore.acs_exception.exceptions import ServerException
+from aliyunsdkalidns.request.v20150109.DescribeDomainRecordsRequest import DescribeDomainRecordsRequest
+from aliyunsdkalidns.request.v20150109.AddDomainRecordRequest import AddDomainRecordRequest
+from aliyunsdkalidns.request.v20150109.UpdateDomainRecordRequest import UpdateDomainRecordRequest
+
+# 配置accessKeyId & accessSecret & RegionID(https://www.alibabacloud.com/help/zh/doc-detail/50086.htm)
+AccessKeyID = "accessKeyId"
+AccessSecret = "accessSecret"
+RegionID = "cn-shenzhen" 
+
+# 配置主域名
+MainDomain = "example.com"
+
+# 配置子域名
+SubDomains = "@"
+
+# 创建AcsClient实例
+client = AcsClient(AccessKeyID, AccessSecret, RegionID)
+
+
+# 获取本地公网IP
+def LocalIP():
+    request = get("https://api.myip.la")
+    response = request.text if request.ok else print(
+        f"Getting failed: {request.text}")
+    return response
+
+
+# 获取解析记录列表
+def RecordList(DomainName):
+    request = DescribeDomainRecordsRequest()
+    request.set_accept_format("json")
+    request.set_DomainName(DomainName)
+    response = client.do_action_with_exception(request)
+    records = json.loads(response)
+    return records
+
+
+# 添加解析记录
+def AddRecords(DomainName, ResolutionRecord, RecordType, RecordValue):
+    request = AddDomainRecordRequest()
+    request.set_accept_format('json')
+    request.set_DomainName(DomainName)
+    request.set_RR(ResolutionRecord)
+    request.set_Type(RecordType)
+    request.set_Value(RecordValue)
+    response = client.do_action_with_exception(request)
+    records = json.loads(response)
+    print(records)
+
+
+# 修改解析记录
+def UpdateRecords(RecordID, ResolutionRecord, RecordType, RecordValue):
+    request = UpdateDomainRecordRequest()
+    request.set_accept_format('json')
+    request.set_RecordId(RecordID)
+    request.set_RR(ResolutionRecord)
+    request.set_Type(RecordType)
+    request.set_Value(RecordValue)
+    response = client.do_action_with_exception(request)
+    records = json.loads(response)
+    print(records)
+
+
+if __name__ == "__main__":
+    records = RecordList(MainDomain)
+    local_ip = LocalIP()
+    record_type = "A"
+    if records.get("TotalCount") == 0:
+        for subdomain in SubDomains:
+            AddRecords(MainDomain, subdomain, record_type, local_ip)
+            print(
+                f"Add resolution record. {record_type} record {subdomain} Default {local_ip}")
+    elif records.get("TotalCount") == 1:
+        subdomain_record = records["DomainRecords"]["Record"][0]
+        resolution_record = subdomain_record.get("RR")
+        record_id = subdomain_record.get("RecordId")
+        remote_ip = subdomain_record.get("Value")
+
+        if remote_ip == local_ip:
+            print(
+                f"The DNS record already exists！the current resolution record {remote_ip} is the same as the local public network IP")
+        else:
+            UpdateRecords(record_id, resolution_record, record_type, local_ip)
+            print(
+                f"Modify resolution record. {record_type} record {resolution_record} Default {remote_ip} changed to {record_type} record {resolution_record} Default {local_ip}")
